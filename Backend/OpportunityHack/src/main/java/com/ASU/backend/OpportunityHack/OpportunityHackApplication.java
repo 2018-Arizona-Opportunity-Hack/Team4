bef66e5d35dd23aad90a41458a49b061ded033c6
Dharmesh Ch <dharmeshch@Dharmeshs-MacBook-Pro.local>
2018-10-20 13:44:18 -0700

    first commit

---
+++ Backend/OpportunityHack/src/main/java/com/ASU/backend/OpportunityHack/OpportunityHackApplication.java
@@ -0,0 +1,12 @@
+package com.ASU.backend.OpportunityHack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportunityHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpportunityHackApplication.class, args);
+	}
+}
