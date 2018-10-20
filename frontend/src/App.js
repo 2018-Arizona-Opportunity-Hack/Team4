6b10a89a427dd3c6c960c5f3fd85e4079d5d0f9d
skodumur <skodumur@asu.edu>
2018-10-20 15:28:31 -0700

    Added Tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
 import Home from './Home/home'
 
 class App extends Component {
--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import '../../node_modules/semantic-ui-css/semantic.min.css';
+import { Label, Menu, Tab } from 'semantic-ui-react'
 
 class Home extends Component {
+    
   render() {
+    const panes = [
+        {
+          menuItem: { key: 'users', icon: 'users', content: 'Users' },
+          render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>,
+        },
+        {
+          menuItem: (
+            <Menu.Item key='messages'>
+              Messages<Label>15</Label>
+            </Menu.Item>
+          ),
+          render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>,
+        },
+      ]
     return (
-     <div>
-         Hello
-     </div>
+         <Tab panes={panes} />
     );
   }
 }
