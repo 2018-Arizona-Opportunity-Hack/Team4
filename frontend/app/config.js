040d27e4bfc88d267ac0a46ccf7f35c3bf23c3e6
skodumur <skodumur@asu.edu>
2018-10-21 13:53:51 -0700

    Update deployed URL

--- frontend/app/config.js
+++ frontend/app/config.js
@@ -1,5 +1,5 @@
 var config = {
-    //url:"http://opportunityhack-env.97hs2hh9th.us-west-2.elasticbeanstalk.com/"
-    url: "http://10.2.19.195:5000/"
+    url:"http://opportunityhack-env.97hs2hh9th.us-west-2.elasticbeanstalk.com/"
+    //url: "http://10.2.19.195:5000/"
 };
 var configData = config;
